354756d89c5f3f01dc0f5c14d68c21eedcc57b85
rexxysasori <13378034+rexxysasori@user.noreply.gitee.com>
2025-07-18 10:53:47 +0800

    deploy.sh

--- workloads/thumbnailer-python/deploy.sh
+++ workloads/thumbnailer-python/deploy.sh
@@ -1,5 +1,1 @@
-func deploy --push=false -v \                                    
-  -e MINIO_ENDPOINT=minio.minio.svc.cluster.local:9000 \
-  -e MINIO_ACCESS_KEY=minioadmin \
-  -e MINIO_SECRET_KEY=minioadmin \  
-  -e MINIO_SECURE=false  
+func deploy --push=false -v -e MINIO_ENDPOINT=minio.minio.svc.cluster.local:9000 -e MINIO_ACCESS_KEY=minioadmin -e MINIO_SECRET_KEY=minioadmin -e MINIO_SECURE=false  
