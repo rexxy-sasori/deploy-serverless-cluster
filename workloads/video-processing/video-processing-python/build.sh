6e18c79b279df9ffe99866e72505b1893112b8a7
rexxysasori <13378034+rexxysasori@user.noreply.gitee.com>
2025-07-21 09:32:13 +0800

    python

--- workloads/video-processing/video-processing-python/build.sh
+++ workloads/video-processing/video-processing-python/build.sh
@@ -1,1 +1,1 @@
-func build -v --base-image=jrottenberg/ffmpeg:7-scratch --builder=host --image=fusion-108:5000/video-processing-golang --push=true --registry-insecure
+func build -v --base-image=jrottenberg/ffmpeg:7-scratch --builder=host --image=fusion-108:5000/video-processing-python --push=true --registry-insecure
